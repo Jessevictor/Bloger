e378d456296036c7acc71c278471e866f557d226
jessevictor <kongoijesse97@gmail.com>
2020-10-01 11:28:50 +0300

    change 404 error

--- config.py
+++ config.py
@@ -13,8 +13,9 @@ class Config:
     
     #  email configurations
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
